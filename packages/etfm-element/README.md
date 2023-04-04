9ce721dc2ca14494fb4743bdb0ebeb36b1cbcabb
lzdjack <1956252575@qq.com>
2023-04-04 22:42:02 +0800

    first commit

---
+++ packages/etfm-element/README.md
@@ -0,0 +1,11 @@
+# `@etfm/etfm-element`
+
+> TODO: description
+
+## Usage
+
+```
+const etfmElement = require('@etfm/etfm-element');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/etfm-element/__tests__/etfm-element.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const etfmElement = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(etfmElement(), 'Hello from etfmElement');
+console.info("etfmElement tests passed");

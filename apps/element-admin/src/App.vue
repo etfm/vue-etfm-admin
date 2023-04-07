b63fe621f9d71b86b29fb40e3a05daca7f846b17
lzdjack <1956252575@qq.com>
2023-04-07 22:35:19 +0800

    feat(element-admin): 增加scssbem方案

--- apps/element-admin/src/App.vue
+++ apps/element-admin/src/App.vue
@@ -6,4 +6,4 @@
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped lang="scss"></style>
--- apps/element-admin/src/main.ts
+++ apps/element-admin/src/main.ts
@@ -2,17 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-// import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import './styles/index.scss'
 
 async function bootstrap() {
   const app = createApp(App)
 
   app.use(createPinia())
   app.use(router)
-
-  // for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  //   app.component(key, component)
-  // }
 
   app.mount('#app')
 }

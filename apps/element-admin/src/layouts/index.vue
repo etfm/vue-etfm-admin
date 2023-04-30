41f9f193849387bfc68c3f13451b5eb11c91115c
lzdjack <1956252575@qq.com>
2023-04-30 18:13:21 +0800

    feat(element-admin): 增加menu

--- apps/element-admin/src/layouts/index.vue
+++ apps/element-admin/src/layouts/index.vue
@@ -3,6 +3,7 @@
   import { ElCheckbox, ElInputNumber, ElRadioGroup, ElRadioButton } from 'element-plus';
   import { reactive } from 'vue';
   import AppLogo from './components/logo/AppLogo.vue';
+  import { BasicMenu } from './components/menu';
 
   defineOptions({
     name: 'Dashboard',
@@ -30,6 +31,7 @@
     <template #side>
       <div>
         <AppLogo />
+        <BasicMenu />
       </div>
     </template>
     <template #side-extra>side-extra</template>

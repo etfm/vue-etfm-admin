245c91c8011ec220612588f20e4bdc79eadc7e7a
lzdjack <1956252575@qq.com>
2023-05-15 21:21:23 +0800

    feat(element-ui): menu

---
+++ packages/element-ui/src/menu/src/BasicMenuItem.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+  import { ElMenuItem } from 'element-plus';
+  import { computed } from 'vue';
+  import BasicMenuItemContent from './BasicMenuItemContent.vue.js';
+import { Menu } from '@/router/types';
+
+  interface Props {
+    /**
+     * 菜单项
+     */
+    menuItem: Menu;
+  }
+
+
+const props = withDefaults(defineProps<Props>(), {})
+
+  /**
+   * 判断是否有子节点，动态渲染 menu-item/sub-menu-item
+   */
+  const hasChildren = computed(() => {
+    return !menuTreeItem.meta?.hideChildrenInMenu &&
+      Reflect.has(menuTreeItem, 'children') &&
+      !!menuTreeItem.children &&
+      menuTreeItem.children.length > 0
+  })
+</script>
+
+<template>
+  <ElMenuItem>
+    <BasicMenuItemContent />
+  </ElMenuItem>
+</template>
+
+<style lang="scss" scoped module></style>
---
+++ packages/element-ui/src/menu/src/BasicSubMenu.vue
@@ -0,0 +1,11 @@
+<template>
+  <BasicMenuItem />
+</template>
+<script lang="ts" setup>
+  import BasicMenuItem from './BasicMenuItem.vue.js';
+
+  defineOptions({
+    name: 'BasicSubMenuItem',
+  });
+</script>
+<style module lang="scss" scoped></style>

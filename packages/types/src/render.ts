4b6ddd907abd77a1f2c9ec1a0d783352083ca9be
lzdjack <1956252575@qq.com>
2023-05-23 09:23:22 +0800

    feat(locale): 初始化多语言插件

--- packages/types/src/render.ts
+++ packages/types/src/render.ts
@@ -2,12 +2,24 @@ import type { App } from 'vue';
 import type { Router } from 'vue-router';
 import type { Pinia } from 'pinia';
 import type { AnyFunction } from './tools';
+import type { I18n } from 'vue-i18n';
 
 export interface IRenderContext {
   rootElement?: App | Element | AnyFunction;
   rootContainer?: App | AnyFunction;
   onRouterCreated?: (opts?: { router: Router }) => void | Promise<void>;
   onPiniaCreated?: (opts?: { pinia: Pinia }) => void | Promise<void>;
-  onAppCreated?: (opts?: { app: App; router: Router; pinia: Pinia }) => void | Promise<void>;
-  onMounted?: (opts: { app: App; router: Router; pinia: Pinia }) => Promise<void> | void;
+  onLocaleCreated?: (opts?: { locale: I18n }) => void | Promise<void>;
+  onAppCreated?: (opts?: {
+    app: App;
+    router: Router;
+    pinia: Pinia;
+    locale: I18n;
+  }) => void | Promise<void>;
+  onMounted?: (opts: {
+    app: App;
+    router: Router;
+    pinia: Pinia;
+    locale: I18n;
+  }) => Promise<void> | void;
 }

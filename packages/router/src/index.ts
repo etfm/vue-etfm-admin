803e8584299f4b9577743e1fd897a416e5a8b0ac
lzdjack <1956252575@qq.com>
2023-06-12 21:17:54 +0800

    fix(router): 更新

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -7,3 +7,9 @@ export * from './usePage';
 export type { IRouterContext, AppRouteRecordRaw, AppRouteModule } from '@etfma/types';
 
 export * from 'vue-router';
+
+export const RouterPlugin = (ctx: any) => {
+  return {
+    init() {},
+  };
+};

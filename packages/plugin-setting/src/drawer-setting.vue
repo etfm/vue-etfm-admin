656897b8b018424a9d6fee9149cc325f6a96537a
lzdjack <1956252575@qq.com>
2023-07-18 21:52:19 +0800

    feat(plugin-setting): 优化setting

--- packages/plugin-setting/src/drawer-setting.vue
+++ packages/plugin-setting/src/drawer-setting.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+  import { ElDivider } from 'element-plus';
   import { BasicDrawer, useDrawerInner } from '@etfma/element-ui';
 
   const [register] = useDrawerInner();
 </script>
 
 <template>
-  <BasicDrawer @register="register" title="测试" />
-</template>
+  <BasicDrawer @register="register" title="项目设置">
+    <ElDivider> 主题 </ElDivider>
 
-<style></style>
+    <ElDivider> 导航栏模式 </ElDivider>
+
+    <ElDivider> 界面功能 </ElDivider>
+
+    <ElDivider> 界面显示 </ElDivider>
+
+    <ElDivider> 动画 </ElDivider>
+  </BasicDrawer>
+</template>
